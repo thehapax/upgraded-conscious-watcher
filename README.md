70519e6399a8450fe042446a182830e836b6768e
thehapax <39811582+thehapax@users.noreply.github.com>
2020-04-22 23:56:02 -0700

    add bot framework

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Telegram Bot for watching power outage urls for changes
 Uses urlwatch to monitors webpages, repository at https://github.com/thp/urlwatch
 
 user driven or channel feeds
+to watch Powergrid us
---
+++ parse_change.py
@@ -0,0 +1,42 @@
+import sys
+from bs4 import BeautifulSoup
+import re
+
+def parse_opml(file):
+    file = sys.argv[1]
+    handler = open(file).read()
+    soup = BeautifulSoup(handler, 'html.parser')
+
+    tables = soup.find_all("table")
+    print(tables)
+    
+    """
+    for link in soup.find_all("table"): #, class_='topfivetable'):
+        try: 
+            print("-------")
+            print(link)
+        except Exception as e:
+            pass
+    """    
+
+def read_lines(fname): 
+    stripped = ''
+    with open(fname) as f:
+        content = f.readlines()
+        for line in content:
+            if re.search('^-', line) or re.search('\@\@', line):
+                continue
+            elif re.search('^\+\+\+', line):
+                result = re.sub('^\+\+\+ \@\t', '', line)
+                stripped += result +"\n"
+            else:
+                stripped += line
+                       
+    return stripped
+
+
+if __name__ == "__main__":
+#    parse_opml(sys.argv[1])
+    parsed_lines = read_lines(sys.argv[1])
+    print(parsed_lines)
+    
