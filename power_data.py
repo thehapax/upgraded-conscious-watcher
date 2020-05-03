f0b1e7390b877544a8ceb90600cf5838822c0ea2
thehapax <39811582+thehapax@users.noreply.github.com>
2020-05-02 23:54:11 -0700

    add methods for getting table data

--- power_data.py
+++ power_data.py
@@ -5,10 +5,10 @@ import pandas as pd
 from us_states import states
 
 main_url = "https://poweroutage.us/"
-state = main_url + "area/state/"
-regions = main_url +  "area/regions"
-california = state + "california"
-#print(states['WA'])
+state_base = main_url + "area/state/"
+regions_url = main_url +  "area/regions"
+#california = state_base + "california"
+print(states['WA'])
 #print(soup.prettify()) # print the parsed data of html
 
 
@@ -16,32 +16,60 @@ def get_top5data():
     html_content = requests.get(main_url).text
     soup = BeautifulSoup(html_content, "lxml")
     #print(soup.title.text)
-
     topfive_page = soup.find("table", attrs={"class" : "topfivetable table-striped"})
     return topfive_page
+
+def get_region_data():
+    html_content = requests.get(regions_url).text
+    soup = BeautifulSoup(html_content, "lxml")
+    region_data = soup.find("table")
+    return region_data
+
+def get_state_data(state):
+    state_url = state_base + state
+    print(state_url)
+    html_content = requests.get(state_url).text
+    soup = BeautifulSoup(html_content, "lxml")
+    print(soup.title.text)
+    state_data = soup.find("div", attrs={"class" : "row col-md-12"})
+    return state_data
+
     
 if __name__ == "__main__":
+    print("\nTOP AREAS FOR OUTAGES:")
     top5 = get_top5data()
     print(top5)
+    
+    print("\nREGIONAL OUTAGES:")
+    region = get_region_data()
+    print(region.text)
+    
+    print("\nSTATE LEVEL DATA")
+    state = get_state_data("california")
+    print("====")
+#    print(state)
+#    sinfo = state.text.strip("\t")
+#    print(sinfo)
+    
     pass
 
+
+
+
+
 """
+    # traversing example:
     top_table_data = topfive_page.find_all("tr")
     print(top_table_data)
-
     for i in top_table_data:
         for item in i.find_all("td"):
-            print(item)
-    #        print(item.text)
+           print(item.text)
+           
 
-
-"""
-    
-    
-"""
 df = pd.DataFrame()
 top_areas = pd.read_html(main_url, header=0)
 top_areas = pd.read_html(regions, header=0)
 print(top_areas[0])
 print(type(top_areas))
+
 """
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient, events, Button
 import yaml
 import logging
+from power_data import get_state_data, get_top5data, get_region_data
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger('telethon').setLevel(level=logging.WARNING)
@@ -30,28 +31,39 @@ async def callback(event):
     print(event.data)
     await event.edit('Thank you for clicking {}!'.format(event.data))
 
-#@client.on(events.NewMessage(pattern='(?i)/start', forwards=False))
-@client.on(events.NewMessage(incoming=True))
+@client.on(events.NewMessage(pattern='(?i)/start', forwards=False))
+#@client.on(events.NewMessage(incoming=True))
 async def new_handler(event):
     if 'hello' in event.raw_text:
         # await event.reply('respond - hi!')
         await client.send_message(event.sender_id, 'A single button, with "clickme" as data',
                         buttons=Button.inline('Click me', b'clickme'))
-
+    else:
+        await client.send_message(event.sender_id, 'Welcome to PowerOutage.US watcher bot')
   
 #@client.on(events.NewMessage)
 @events.register(events.NewMessage(incoming=True, outgoing=False))
 async def handler(event):
     print(event.raw_text)
-#    print(event.geo)
-#   print(event.location)
-    if 'alerts' in event.raw_text:
-        await client.send_message(event.sender_id, 'Get Alerts', buttons=[
-            Button.text('10k-50k (Yellow Alert)', resize=True, single_use=True),
-            Button.text('50k-100k (Orange Alert)', resize=True, single_use=True),
+    if '/alerts' in event.raw_text:
+        await client.send_message(event.sender_id, 'Get Updates', buttons=[
+            Button.text('Top 5 Outages', resize=True, single_use=True),
+            Button.text('Regional Outages', resize=True, single_use=True),
             Button.text('>100k Outages (Red Alert)', resize=True, single_use=True)
         ])
-        
+    elif 'Top 5 Outages' in event.raw_text:
+        #msg = "<b>html header</b>"
+        msg = get_top5data()
+        print(msg.text)
+        await client.send_message(event.sender_id, msg.text)
+    elif 'Regional Outages' in event.raw_text:
+        msg = get_region_data()
+        print(msg.text)
+        await client.send_message(event.sender_id, msg.text)
+    elif '>100k Outages (Red Alert)' in event.raw_text:
+        msg = "<b>placeholder text</b>"        
+        await client.send_message(event.sender_id, msg)
+                
 
 @client.on(events.InlineQuery)
 #@events.register(events.InlineQuery)
